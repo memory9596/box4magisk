4b0b8f6039c1fae5884def713aec1941715f6642
memory9596 <29706683+memory9596@users.noreply.github.com>
2024-04-29 19:06:32 +0800

    add AdguardHome and mosdns

--- box/scripts/start.sh
+++ box/scripts/start.sh
@@ -27,12 +27,17 @@ wait_until_login(){
 wait_until_login
 
 rm ${pid_file}
+rm ${mos_pid_file}
+rm ${adg_pid_file}
+
 mkdir -p ${run_path}
 
 if [ ! -f ${box_path}/manual ] && [ ! -f ${module_dir}/disable ] ; then
   mv ${run_path}/run.log ${run_path}/run.log.bak
   mv ${run_path}/run_error.log ${run_path}/run_error.log.bak
 
-  ${scripts_dir}/box.service start >> ${run_path}/run.log 2>> ${run_path}/run_error.log && \
-  ${scripts_dir}/box.tproxy enable >> ${run_path}/run.log 2>> ${run_path}/run_error.log
+  ${scripts_dir}/box.service start >> ${run_path}/run.log 2>> ${run_path}/run_error.log
+  if [ "${bin}" = "enable" ] ; then
+    ${scripts_dir}/box.tproxy enable >> ${run_path}/run.log 2>> ${run_path}/run_error.log
+  fi
 fi
